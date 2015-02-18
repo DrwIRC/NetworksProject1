ea629c0ce9311f0f8c42b3794d90b9adf7e13e0e
Smores42 <Smores42@users.noreply.github.com>
2015-02-17 22:51:23 -0500

    began code

---
+++ mainPython.py
@@ -0,0 +1,17 @@
+from socket import *
+
+serverName = input('Name of Host to Connect to: ')
+serverPort = 12000
+
+#Creates the client socket 
+clientSocket = socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+#TCP Connection
+clientSocket.connect(serverName, serverPort)
+
+
+#What Object would you like 
+objectName = input('What Object do you want to download?: ')
+
+
+#Send a  Sentence (GET?)
